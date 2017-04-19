813a2c5d4c7f292d4154f74e0b1273da8ae64c98
中安佑一 <yuichi.nakayasu@gmail.com>
2017-04-20 07:03:31 +0900

    ApiAccessorを実装

--- ios/Todo-Assignment/Sources/Networks/ApiAccessor.swift
+++ ios/Todo-Assignment/Sources/Networks/ApiAccessor.swift
@@ -3,3 +3,48 @@
 // Created by NAKAYASU Yuichi
 // - * - * - * - * - * - * - * - * - * - * - * - * - * - * - *
 import UIKit
+import Alamofire
+import SwiftyJSON
+
+/// APIリクエストプロトコル
+class ApiAccessor {
+    
+    let baseURL: String
+    
+    init(baseURL: String) {
+        self.baseURL = baseURL
+    }
+    
+    func request<T: ApiRequestable>(_ requestable: T, handler: @escaping (T.Response?, ApiResult) -> ()) {
+        let urlString = "\(self.baseURL)\(requestable.endpoint)"
+        
+        let config = URLSessionConfiguration.default
+        if let timeoutInterval = requestable.requestTimeoutInterval {
+            config.timeoutIntervalForRequest = timeoutInterval
+        }
+        
+        let request = Alamofire.request(
+            urlString,
+            method:     requestable.method,
+            parameters: requestable.parameters,
+            encoding:   JSONEncoding.default,
+            headers:    requestable.headers
+        )
+        
+        request.responseJSON() { data in
+            let result = ApiResult()
+            result.statusCode = data.response?.statusCode ?? 0
+            result.requestedURL = data.request?.url?.absoluteString ?? ""
+            
+            if let err = data.result.error {
+                result.error = err
+                handler(nil, result)
+            }
+            guard let value = data.result.value else {
+                handler(nil, result)
+                return
+            }
+            handler(requestable.parse(JSON(value)), result)
+        }
+    }
+}
