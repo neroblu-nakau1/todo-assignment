8ad9517260bffafb2ee900a2690449090424925c
中安佑一 <yuichi.nakayasu@gmail.com>
2017-04-20 12:17:56 +0900

    ナビゲーションバーの色を設定

--- ios/Todo-Assignment/Sources/AppDelegate.swift
+++ ios/Todo-Assignment/Sources/AppDelegate.swift
@@ -10,6 +10,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        self.configureNavigationBar()
         return true
+    }
+    
+    private func configureNavigationBar() {
+        UINavigationBar.appearance().tintColor    = UIColor.white
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.596, green: 0.725, blue: 0.976, alpha: 1.0)
     }
 }
