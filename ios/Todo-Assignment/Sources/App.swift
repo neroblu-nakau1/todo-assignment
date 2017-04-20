316f6ddaea4ef91ab0352a685863bea9410a4d03
中安佑一 <yuichi.nakayasu@gmail.com>
2017-04-20 12:17:40 +0900

    汎用的なprint関数を定義

--- ios/Todo-Assignment/Sources/App.swift
+++ ios/Todo-Assignment/Sources/App.swift
@@ -15,3 +15,14 @@ extension App {
     /// モデル
     class Model {}
 }
+
+/// オプショナルの値も渡せるprint関数
+/// - parameter items: 値(可変長引数)
+/// - parameter separator: セパレータ文字
+/// - parameter terminator: 終了文字
+public func print(_ items: Any?..., separator: String = ", ", terminator: String = "\n") {
+    for (i, item) in items.enumerated() {
+        let t = (i == items.indices.last!) ? terminator : separator
+        print(item ?? NSNull(), terminator: t)
+    }
+}
