b5218eaf6dbdf4f23ad2f767dda48eaefaaa6a8b
yuichi <yuichi.nakayasu@gmail.com>
2017-04-25 11:34:19 +0900

    日付処理を強化

--- ios/Todo-Assignment/Sources/Libs/Date+Util.swift
+++ ios/Todo-Assignment/Sources/Libs/Date+Util.swift
@@ -5,22 +5,78 @@
 import UIKit
 
 extension Date {
-    
-    static func today() -> Date {
-        return Date().zeroClock()
-    }
-    
-    func zeroClock(addDay: Int = 0) -> Date {
-        var comps = DateComponents()
-        comps.year   = self.calendar.component(.year,  from: self)
-        comps.month  = self.calendar.component(.month, from: self)
-        comps.day    = self.calendar.component(.day,   from: self) + addDay
-        comps.hour   = 0
-        comps.minute = 0
-        comps.second = 0
-        return self.calendar.date(from: comps)!
-    }
-    
+	
+	/// 各日付時刻情報から新しいDateオブジェクトを生成する
+	/// - parameter year: 年
+	/// - parameter month: 月
+	/// - parameter day: 日
+	/// - parameter hour: 時
+	/// - parameter minute: 分
+	/// - parameter second: 秒
+	/// - returns: 新しいDateオブジェクト
+	static func create(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+		let now = Date()
+		var comps = DateComponents()
+		comps.year   = year   ?? now.year
+		comps.month  = month  ?? now.month
+		comps.day    = day    ?? now.day
+		comps.hour   = hour   ?? now.hour
+		comps.minute = minute ?? now.minute
+		comps.second = second ?? now.second
+		return self.calendar.date(from: comps)!
+	}
+	
+	/// 今日の0時0分0秒のDateオブジェクトを生成する
+	/// - returns: 新しいDateオブジェクト
+	static func today() -> Date {
+		return Date().zeroClock()
+	}
+	
+	/// 明日の0時0分0秒のDateオブジェクトを生成する
+	/// - returns: 新しいDateオブジェクト
+	static func tomorrow() -> Date {
+		return Date().zeroClock(addDay: 1)
+	}
+	
+	/// 明後日の0時0分0秒のDateオブジェクトを生成する
+	/// - returns: 新しいDateオブジェクト
+	static func dayAfterTomorrow() -> Date {
+		return Date().zeroClock(addDay: 2)
+	}
+	
+	/// 各日付時刻情報に値を追加して新しいDateオブジェクトを生成する
+	/// - parameter year: 追加する年数
+	/// - parameter month: 追加する月数
+	/// - parameter day: 追加する日数
+	/// - parameter hour: 追加する時数
+	/// - parameter minute: 追加する分数
+	/// - parameter second: 追加する秒数
+	/// - returns: 新しいDateオブジェクト
+	func addedDate(year: Int = 0, month: Int = 0, day: Int = 0, hour: Int = 0, minute: Int = 0, second: Int = 0) -> Date {
+		var comps = DateComponents()
+		comps.year   = self.year   + year
+		comps.month  = self.month  + month
+		comps.day    = self.day    + day
+		comps.hour   = self.hour   + hour
+		comps.minute = self.minute + minute
+		comps.second = self.second + second
+		return self.calendar.date(from: comps)!
+	}
+
+	/// 0時0分0病に設定された新しいDateオブジェクトを生成する
+	/// - parameter addDay: 追加する日数
+	/// - returns: 新しいDateオブジェクト
+	func zeroClock(addDay: Int = 0) -> Date {
+		var comps = DateComponents()
+		comps.year   = self.year
+		comps.month  = self.month
+		comps.day    = self.day + addDay
+		comps.hour   = 0
+		comps.minute = 0
+		comps.second = 0
+		return self.calendar.date(from: comps)!
+	}
+	
     /// 年
     var year: Int { return self.calendar.component(.year, from: self) }
     
@@ -42,37 +98,35 @@ extension Date {
     /// 週
     var week: String {
         let index = self.calendar.component(.weekday, from: self) - 1
-        return self.dateFormatter("").shortWeekdaySymbols[index]
+        return self.dateFormatter().shortWeekdaySymbols[index]
     }
-    
-    func localizedName(_ symbols: (DateFormatter) -> [String]!, index: Int) -> String {
-        let fmt = DateFormatter()
-        fmt.calendar = self.calendar
-        fmt.locale = Locale.current
-        return symbols(fmt)[index]
-    }
-    
-    
+	
+    /// 日付が今日かどうか
     var isToday: Bool {
-        return self.zeroClock() == Date().zeroClock()
+        return self.zeroClock() == Date.today()
     }
-    
+	
+	/// 日付が明日かどうか
     var isTomorrow: Bool {
-        return self.zeroClock() == Date().zeroClock(addDay: 1)
+        return self.zeroClock() == Date.tomorrow()
     }
-    
+	
+	/// 日付が明後日かどうか
     var isDayAfterTomorrow: Bool {
-        return self.zeroClock() == Date().zeroClock(addDay: 2)
+        return self.zeroClock() == Date.dayAfterTomorrow()
     }
-    
+	
+	/// 日付が今月かどうか
     var isThisMonth: Bool {
-        return self.isThisYear && self.zeroClock().month  == Date().zeroClock().month
+        return self.isThisYear && self.zeroClock().month == Date().zeroClock().month
     }
-    
+	
+	/// 日付が今年かどうか
     var isThisYear: Bool {
-        return self.zeroClock().year  == Date().zeroClock().year
+        return self.zeroClock().year == Date().zeroClock().year
     }
-    
+	
+	/// 日付の表示用文字列
     var dateString: String {
         if self.isToday {
             return "今日"
@@ -83,25 +137,41 @@ extension Date {
         } else if self.isThisMonth {
             return "今月\(self.day)日(\(self.week))"
         } else if self.isThisYear {
-            return self.dateFormatter("M月d日").string(from: self) + "(\(self.week))"
+            return self.formattedString("M月d日") + "(\(self.week))"
         } else {
-            return self.dateFormatter("Y年M月d日").string(from: self) + "(\(self.week))"
+            return self.formattedString("Y年M月d日") + "(\(self.week))"
         }
     }
-    
+	
+	/// 時刻の表示用文字列
     var timeString: String {
-        return self.dateFormatter("HH時mm分").string(from: self)
+        return self.formattedString("HH時mm分")
     }
-    
+	
+	/// 日時の表示用文字列
+	var string: String {
+		return "\(self.dateString) \(self.timeString)"
+	}
+	
+	/// フォーマット文字列に基づいた日付文字列を返す
+	/// - parameter format: フォーマット文字列
+	/// - returns: フォーマットされた文字列
+	func formattedString(_ format: String = "") -> String {
+		return self.dateFormatter(format).string(from: self)
+	}
+	
+	/// カレンダー(グレゴリアン歴)
     static var calendar: Calendar {
         return Calendar(identifier: .gregorian)
     }
-    
-    private var calendar: Calendar {
+	
+	/// カレンダー(グレゴリアン歴)
+    var calendar: Calendar {
         return Date.calendar
     }
-    
-    private func dateFormatter(_ format: String) -> DateFormatter {
+	
+	/// 日付フォーマッタ
+    func dateFormatter(_ format: String = "") -> DateFormatter {
         let fmt = DateFormatter()
         fmt.calendar   = self.calendar
         fmt.dateFormat = format
@@ -109,11 +179,8 @@ extension Date {
         fmt.timeZone   = TimeZone.current
         return fmt
     }
-    
-    var description: String {
-        return self.dateFormatter("yyyy/MM/dd HH:mm:ss").string(from: self)
-    }
-    
+	
+	/// 日付コンポーネント
     var components: DateComponents {
         return self.calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: self)
     }
