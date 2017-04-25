ebc5fb17fc8acd00bbe677c750e6b859f18a0b10
yuichi <yuichi.nakayasu@gmail.com>
2017-04-25 13:06:47 +0900

    AppDelegateからナビゲーションの設定を削除

--- ios/Todo-Assignment/Sources/AppDelegate.swift
+++ ios/Todo-Assignment/Sources/AppDelegate.swift
@@ -10,13 +10,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        App.Model.Task.fixture()
-        self.configureNavigationBar()
         return true
-    }
-    
-    private func configureNavigationBar() {
-        UINavigationBar.appearance().tintColor    = UIColor.white
-        UINavigationBar.appearance().barTintColor = UIColor(red: 0.596, green: 0.725, blue: 0.976, alpha: 1.0)
     }
 }
