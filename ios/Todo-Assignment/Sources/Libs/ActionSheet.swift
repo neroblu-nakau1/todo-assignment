ec40af9d80405a2a6d5d6dfd072d4ba6b5d5935f
yuichi <yuichi.nakayasu@gmail.com>
2017-04-25 16:47:02 +0900

    アクションシートとアラートの強化

--- ios/Todo-Assignment/Sources/Libs/ActionSheet.swift
+++ ios/Todo-Assignment/Sources/Libs/ActionSheet.swift
@@ -29,21 +29,46 @@ extension UIAlertAction {
     }
 }
 
-/// アクションシート用の拡張
+
 extension UIAlertController {
-    
-    class func showActionSheet(_ controller: UIViewController, title: String? = nil, message: String? = nil, actions: [UIAlertAction]) {
-        if actions.isEmpty { return }
-        
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-        actions.forEach { alert.addAction($0) }
-        controller.present(alert, animated: true, completion: nil)
-    }
-    
-    class func showDeleteConfirmActionSheet(_ controller: UIViewController, confirmed: @escaping ()->()) {
-        self.showActionSheet(controller, title: "削除しますか?", message: nil, actions: [
-            UIAlertAction(destructive: "削除します", confirmed),
-            UIAlertAction(cancel: "キャンセル"),
-            ])
-    }
+	
+	class func showDeleteConfirmActionSheet(_ controller: UIViewController, confirmed: @escaping ()->()) {
+		self.showActionSheet(controller, title: "削除しますか?", message: nil, actions: [
+			UIAlertAction(destructive: "削除します", confirmed),
+			UIAlertAction(cancel: "キャンセル"),
+			])
+	}
+	
+	class func showOKAlert(_ controller: UIViewController, message: String) {
+		self.showActionSheet(controller, title: nil, message: message, actions: [
+			UIAlertAction(destructive: "OK"),
+			])
+	}
 }
+
+/// UIAlertController拡張
+extension UIAlertController {
+	
+	/// アラートを表示する
+	/// - parameter controller: 表示を行うビューコントローラ
+	/// - parameter title: タイトル文言
+	/// - parameter message: メッセージ文言
+	/// - parameter actions: UIAlertActionの配列
+	/// - parameter style: UIAlertControllerStyle
+	private class func show(_ controller: UIViewController, title: String?, message: String?, actions: [UIAlertAction], style: UIAlertControllerStyle) {
+		if actions.isEmpty { return }
+		
+		let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+		actions.forEach { alert.addAction($0) }
+		controller.present(alert, animated: true, completion: nil)
+	}
+	
+    fileprivate class func showActionSheet(_ controller: UIViewController, title: String? = nil, message: String? = nil, actions: [UIAlertAction]) {
+		self.show(controller, title: title, message: message, actions: actions, style: .actionSheet)
+    }
+	
+	fileprivate class func showAlert(_ controller: UIViewController, title: String? = nil, message: String? = nil, actions: [UIAlertAction]) {
+		self.show(controller, title: title, message: message, actions: actions, style: .alert)
+	}
+}
+
