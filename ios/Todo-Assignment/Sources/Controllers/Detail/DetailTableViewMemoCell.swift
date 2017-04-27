d7cba5e71d6738326a8dc3211e100c9b6e1b0260
中安佑一 <yuichi.nakayasu@gmail.com>
2017-04-27 14:30:15 +0900

    メモ欄のプレースホルダ実装方法を修正

--- ios/Todo-Assignment/Sources/Controllers/Detail/DetailTableViewMemoCell.swift
+++ ios/Todo-Assignment/Sources/Controllers/Detail/DetailTableViewMemoCell.swift
@@ -7,12 +7,13 @@ import UIKit
 class DetailTableViewMemoCell: DetailTableViewCell {
     
     @IBOutlet fileprivate weak var memoLabel: UILabel!
-    @IBOutlet fileprivate weak var placeholderLabel: UILabel!
     
     override var task: Task! {
         didSet {
-            self.memoLabel.text = self.task.memo
-            self.placeholderLabel.isHidden = !self.task.memo.isEmpty
+            let fill = !self.task.memo.isEmpty
+            
+            self.memoLabel.text      = fill ? self.task.memo : "メモを入力…"
+            self.memoLabel.textColor = fill ? UIColor.darkText : UIColor.lightGray
         }
     }
 }
