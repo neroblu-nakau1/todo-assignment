1eedaf7ff2483131da23c2540e67a24770b01c27
中安佑一 <yuichi.nakayasu@gmail.com>
2017-04-27 11:34:57 +0900

    ボタンの拡張を汎用処理に移行

--- ios/Todo-Assignment/Sources/Libs/Utilities.swift
+++ ios/Todo-Assignment/Sources/Libs/Utilities.swift
@@ -84,3 +84,20 @@ public extension Int {
         return min + r
     }
 }
+
+/// UIButton拡張
+extension UIButton {
+    
+    /// ボタンタイトル(通常時)
+    var title: String {
+        get    { return self.title(for: .normal) ?? "" }
+        set(v) { self.setTitle(v, for: .normal) }
+    }
+    
+    /// ボタン画像(通常時)
+    var image: UIImage? {
+        get    { return self.image(for: .normal) }
+        set(v) { self.setImage(v, for: .normal) }
+    }
+}
+
